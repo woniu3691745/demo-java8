e590c2e95e086dfb49dbe1685bc0a114400ccee1
lidl <3691745@qq.com>
2017-08-03 16:11:09 +0800

    update

---
+++ src/main/java/com/example/thread/pattern/StaticSingleton.java
@@ -0,0 +1,24 @@
+package com.example.thread.pattern;
+
+/**
+ * Created by lidongliang on 2017/7/27.
+ * Singleton pattern
+ */
+public class StaticSingleton {
+
+    private StaticSingleton() {
+        System.out.println("StaticSingleton is create");
+    }
+
+    private static class SingletonHolder {
+        private static StaticSingleton instance = new StaticSingleton();
+    }
+
+    public static StaticSingleton getInstance() {
+        return SingletonHolder.instance;
+    }
+
+    public static void main(String[] args) {
+        StaticSingleton.getInstance();
+    }
+}
--- src/main/java/com/example/thread/threadPool/ExecutorsThreadPool.java
+++ src/main/java/com/example/thread/threadPool/ExecutorsThreadPool.java
@@ -1,7 +1,5 @@
 package com.example.thread.threadPool;
 
-import java.util.Collection;
-import java.util.List;
 import java.util.concurrent.*;
 
 /**
