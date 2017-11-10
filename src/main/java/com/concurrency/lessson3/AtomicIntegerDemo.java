ff8ee1b8937494410655ce5122ec873948f61193
lidl <3691745@qq.com>
2017-11-10 16:47:58 +0800

    Java高并发程序设计 第三章

---
+++ src/main/java/com/concurrency/lessson3/AtomicIntegerDemo.java
@@ -0,0 +1,40 @@
+package com.concurrency.lessson3;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 描述:
+ * <p>
+ * 无锁的线程安全整数
+ *
+ * @author lidongliang
+ * @create 2017-11-10 15:48
+ */
+public class AtomicIntegerDemo {
+
+    static AtomicInteger i = new AtomicInteger();
+
+    public static class AddThread implements Runnable {
+
+        @Override
+        public void run() {
+            for (int j = 0; j < 1000; j++) {
+                i.incrementAndGet();    // 加1
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread[] ts = new Thread[10];
+        for (int j = 0; j < 10; j++) {
+            ts[j] = new Thread(new AddThread());
+        }
+        for (int k = 0; k < 10; k++) {
+            ts[k].start();
+        }
+        for (int k = 0; k < 10; k++) {
+            ts[k].join();
+        }
+        System.out.println(i);
+    }
+}
---
+++ src/main/java/com/concurrency/lessson3/DeadLock.java
@@ -0,0 +1,65 @@
+package com.concurrency.lessson3;
+
+/**
+ * 描述:
+ * 死锁的线程不占用CPU
+ *
+ * @author lidongliang
+ * @create 2017-11-10 16:36
+ */
+public class DeadLock extends Thread {
+
+    private Object tool;
+    private static final Object fork1 = new Object();
+    private static final Object fork2 = new Object();
+
+    private DeadLock(Object obj) {
+        this.tool = obj;
+        if (tool == fork1) {
+            this.setName("哲学家A");
+        }
+        if (tool == fork2) {
+            this.setName("哲学家B");
+        }
+    }
+
+    @Override
+    public void run() {
+        if (tool == fork1) {
+            synchronized (fork1) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (fork2) {
+                    System.out.println("哲学家A开始吃饭了");
+                }
+            }
+
+        }
+        if (tool == fork2) {
+            synchronized (fork2) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (fork1) {
+                    System.out.println("哲学家B开始吃饭了");
+                }
+            }
+
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        DeadLock 哲学家A = new DeadLock(fork1);
+        DeadLock 哲学家B = new DeadLock(fork2);
+
+        哲学家A.start();
+        哲学家B.start();
+        Thread.sleep(1000);
+
+    }
+}
