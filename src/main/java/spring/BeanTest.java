d2cd611e03e38b05c87f914ae5c8820e09d18c81
lidl <3691745@qq.com>
2017-12-26 17:48:12 +0800

    add spring learn

---
+++ src/main/java/spring/BeanTest.java
@@ -0,0 +1,14 @@
+package spring;
+
+/**
+ * 描述:
+ *
+ * @author lidongliang
+ * @create 2017-12-26 16:09
+ */
+public class BeanTest {
+
+    void SyaHello() {
+        System.out.println("welcome to me!");
+    }
+}
