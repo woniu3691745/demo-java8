a2ba614aa7b0e2f6782d7174c36c434fa3424626
lidl <3691745@qq.com>
2018-02-05 09:22:31 +0800

    add 负载均衡

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,15 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import spring.bean.BeanFirst;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * 描述:
@@ -10,6 +19,7 @@ import spring.bean.BeanFirst;
  */
 public class Test {
 
+
     public static void main(String[] args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
 //        java.util.Properties properties = (java.util.Properties)ctx.getBean("jdbcConfiguration");
@@ -17,6 +27,14 @@ public class Test {
         BeanFirst beanFirst = (BeanFirst)ctx.getBean("beanFirst");
         beanFirst.getFirstBean();
         Test test = new Test();
+    }
+
+
+    @RequestMapping("/something")
+    public ResponseEntity<String> handle(HttpEntity<byte[]> requestEntity) throws UnsupportedEncodingException {
+        String requestHeader = requestEntity.getHeaders().getFirst("MyRequestHeader");
+        byte[] requestBody = requestEntity.getBody();
+        return new ResponseEntity(HttpStatus.OK);
     }
 
 }
